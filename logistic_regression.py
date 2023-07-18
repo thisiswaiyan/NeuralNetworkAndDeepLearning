5277ae3b73ca15cafdb1a498e268bfe24db06a09
thisiswaiyan <111344227+thisiswaiyan@users.noreply.github.com>
2023-07-18 09:51:10 +0630

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -181,23 +181,23 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     return d
 
 
-#logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
+logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
 
 # predict the cat = 1 or not 0
-#index = 2
-#plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
-#print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[int(logistic_regression_model['Y_prediction_test'][0,index])].decode("utf-8") +  "\" picture.")
+index = 2
+plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
+print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[int(logistic_regression_model['Y_prediction_test'][0,index])].decode("utf-8") +  "\" picture.")
 
-#learning_rates = [0.01, 0.001, 0.0001]
-#models = {}
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
 
-#for lr in learning_rates:
-    #print ("Training a model with learning rate: " + str(lr))
-    #models[str(lr)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=lr, print_cost=False)
-    #print ('\n' + "-------------------------------------------------------" + '\n')
+for lr in learning_rates:
+    print ("Training a model with learning rate: " + str(lr))
+    models[str(lr)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=lr, print_cost=False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
 
-#for lr in learning_rates:
-    #plt.plot(np.squeeze(models[str(lr)]["costs"]), label=str(models[str(lr)]["learning_rate"]))
+for lr in learning_rates:
+    plt.plot(np.squeeze(models[str(lr)]["costs"]), label=str(models[str(lr)]["learning_rate"]))
 
 #plt.ylabel('cost')
 #plt.xlabel('iterations (hundreds)')
